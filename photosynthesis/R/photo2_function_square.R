6c3a3473b245e61c2629891b8958ff105eeebfcb
Marife Anunciado <Marife Anunciado>
2017-05-05 08:01:55 -0400

    removed the extraneous code value_square

---
+++ photosynthesis/R/photo2_function_square.R
@@ -0,0 +1,11 @@
+#' Squares function
+#' This functions takes the squares of the values.
+#' @param x
+#' @return value
+#' @export
+#' @examples
+
+value_square <- function(x)
+  {
+  return(x^2)
+}
